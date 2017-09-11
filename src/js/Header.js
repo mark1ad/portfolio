e244e0a8b1b3cacffae82efe85405b602576c9f6
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-09-11 10:09:22 -0600

    Began work on header nav bar

--- src/js/Header.js
+++ src/js/Header.js
@@ -15,6 +15,25 @@ const Header = (props) => {
           During my career as a software engineer I&rsquo;ve worked on projects for IC layout design, GIS applications for utility companies, and full stack web development. My skills include Ruby on Rails, JavaScript, SQL, CSS/SASS, HTML5, AngularJS, C#, and .Net.
         </p>
       </div>
+
+      <nav className="navbar navbar-expand navbar-light bg-light">
+
+        <div className="container">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="">Centered nav only <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Disabled</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+
     </div>
   )
 }
