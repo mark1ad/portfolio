92e6efa97dac533c6589079ee029375544465335
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-09-21 09:41:11 -0600

    Pass project site png to Project component

--- src/App.js
+++ src/App.js
@@ -8,68 +8,49 @@ class App extends Component {
   projects() {
     return (
       <div className="projects">
+        <Project title="Art From Light"
+          href="https://art-from-light.herokuapp.com/"
+          src={require("./images/art-from-light.png")}
+          alt="Art From Light Website">
 
-        <div className="row">
-          <div className="col-sm-5">
-            <Project title="Art From Light" href="https://art-from-light.herokuapp.com/">
-              <p>
-                A site where photographers can share their work.
-              </p>
-              <p>
-                Ruby on Rails, postgresql, AngularJS, Express.js, JavaScript, Postgresql and CSS/SASS. Photos are stored on <a href="http://cloudinary.com" target="_blank" rel="noopener noreferrer">Cloudinary</a>
-                . <a href="https://github.com/mark1ad/art_from_light_frontend" target="_blank" rel="noopener noreferrer">Client GitHub repository</a> and <a href="https://github.com/mark1ad/art_from_light_api" target="_blank" rel="noopener noreferrer">Server GitHub respository</a>.
-              </p>
-            </ Project>
-          </div>
-          <div className="col-sm-7">
-            <a href="https://art-from-light.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-              <img src={require("./images/art-from-light.png")} alt="Art From Light Website"/>
-            </a>
-          </div>
-        </div>
+          <p>
+            A site where photographers can share their work.
+          </p>
+          <p>
+            Ruby on Rails, postgresql, AngularJS, Express.js, JavaScript, Postgresql and CSS/SASS. Photos are stored on <a href="http://cloudinary.com" target="_blank" rel="noopener noreferrer">Cloudinary</a>
+            . <a href="https://github.com/mark1ad/art_from_light_frontend" target="_blank" rel="noopener noreferrer">Client GitHub repository</a> and <a href="https://github.com/mark1ad/art_from_light_api" target="_blank" rel="noopener noreferrer">Server GitHub respository</a>.
+          </p>
+        </ Project>
 
-        <div className="row">
-          <div className="col-sm-5">
-            <Project title="Open Highway" href="https://open-highway.herokuapp.com/">
-              <p>
-                Plan the ultimate roadtrip!
-              </p>
+        <Project title="Open Highway"
+          href="https://open-highway.herokuapp.com/"
+          src={require("./images/open-highway.png")}
+          alt="Open Highway Website">
 
-              <p>
-                Ruby on Rails, postgresql, AngularJS, Express.js, JavaScript, Postgresql and CSS/SASS.
-                <a href="https://github.com/mark1ad/roadtrip_app_frontend" target="_blank" rel="noopener noreferrer">Client GitHub respository</a> and <a href="https://github.com/mark1ad/roadtrip_app_api" target="_blank" rel="noopener noreferrer">Server GitHub respository</a>.
-              </p>
-            </Project>
-          </div>
-          <div className="col-sm-7">
-            <a href="https://open-highway.herokuapp.com/"  target="_blank" rel="noopener noreferrer">
-              <img src={require("./images/open-highway.png")} alt="Open Highway Website"/>
-            </a>
-          </div>
-        </div>
+          <p>
+            Plan the ultimate roadtrip!
+          </p>
 
-        <div className="row">
-          <div className="col-sm-5">
-            <Project title="Civilized Coffee" href="https://infinite-shelf-28534.herokuapp.com/">
-              <p>
-                Manage your own chain of coffee shops.
-              </p>
-              <p>
-                Node.js, Mongoose, Mongo, Express, EJS and Bootstrap.
-                <a href="https://github.com/mark1ad/coffeeshop" target="_blank" rel="noopener noreferrer">GitHub repository</a>.
-              </p>
-            </Project>
-          </div>
-          <div className="col-sm-7">
-            <a href="https://infinite-shelf-28534.herokuapp.com/"  target="_blank" rel="noopener noreferrer">
-              <img src={require("./images/civilized-coffee.png")} alt="Open Highway Website"/>
-            </a>
-          </div>
-        </div>
+          <p>
+            Ruby on Rails, postgresql, AngularJS, Express.js, JavaScript, Postgresql and CSS/SASS.
+            <a href="https://github.com/mark1ad/roadtrip_app_frontend" target="_blank" rel="noopener noreferrer">Client GitHub respository</a> and <a href="https://github.com/mark1ad/roadtrip_app_api" target="_blank" rel="noopener noreferrer">Server GitHub respository</a>.
+          </p>
+        </Project>
 
+        <Project title="Civilized Coffee"
+          href="https://infinite-shelf-28534.herokuapp.com/"
+          src={require("./images/civilized-coffee.png")}
+          alt="Open Highway Website">
+
+          <p>
+            Manage your own chain of coffee shops.
+          </p>
+          <p>
+            Node.js, Mongoose, Mongo, Express, EJS and Bootstrap. <a href="https://github.com/mark1ad/coffeeshop" target="_blank" rel="noopener noreferrer">GitHub repository</a>.
+          </p>
+        </Project>
       </div>
-  )
-
+    )
   }
 
   render() {
@@ -78,12 +59,8 @@ class App extends Component {
         <Header />
 
         <div className="container">
-          { /* <div className="inner-container"> */ }
-
-              <h2 className="projHead">Projects</h2>
-            { this.projects() }
-
-          { /* </div> */ }
+          <h2 className="projHead">Projects</h2>
+          { this.projects() }
         </div>
       </div>
     );
--- src/js/Project.js
+++ src/js/Project.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Project extends Component {
   render() {
     return (
-      <div>
-        <div className="projLink">
-          <a href={this.props.href} target="_blank">{this.props.title}</a>
+      <div className='row'>
+        <div className="col-md-5">
+          <div className="projLink">
+            <a href={this.props.href} target="_blank"  rel="noopener noreferrer">{this.props.title}</a>
+          </div>
+
+          <div className="projDesc">
+            {this.props.children}
+          </div>
         </div>
-        <div className="projDesc">
-          {this.props.children}
+
+        <div className="col-md-7">
+          <a href={this.props.href} target="_blank"  rel="noopener noreferrer">
+            <img src={this.props.src} alt={this.props.alt}/>
+          </a>
         </div>
       </div>
     )
   }
 }
+
+Project.propTypes = {
+  href: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
 
 export default Project;
