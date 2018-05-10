1be343ccebfbf8cf5d7ee43effdab067037be1d0
Mark De May <markd@frii.com>
2018-05-10 08:54:19 -0600

    Reorganized files. Stubbed Home component.

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div id="home" className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6"></div>
+          <div className="col-sm-6">
+            <h1>Mark De May</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
