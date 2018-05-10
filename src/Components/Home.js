3df96581ff99e47a3d0762ebffd61e8ad602ffb9
Mark De May <markd@frii.com>
2018-05-10 10:45:25 -0600

    Home component finished.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div id="home" className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6"></div>
-          <div className="col-sm-6">
-            <h1>Mark De May</h1>
-          </div>
-        </div>
+      <div id="home-text" className="container">
+        <h1>Mark De May</h1>
+        <h2>Web Developer</h2>
       </div>
     </div>
   )
