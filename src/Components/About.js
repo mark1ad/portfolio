53c0d15f92b66592ea5096792c8ef109c59f1bcd
Mark De May <markd@frii.com>
2018-05-12 10:11:26 -0600

    About layout

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,18 +4,26 @@ import Skill from './Skill';
 const About = () => {
   return (
     <section id="about" className="section">
-      <div className="container inner-section">
+      <div className="container section-inner about-inner">
         <h1>About Me</h1>
-        <p>During my career as a software engineer I’ve worked on projects for IC layout design automation, GIS applications for utility companies, and full stack web development. My skills include Ruby on Rails, JavaScript, SQL, CSS/SASS, HTML5, AngularJS, C#, and .Net.</p>
+        <p>
+          During my career as a software engineer I’ve worked on projects for IC layout design automation, GIS applications for utility companies, and full stack web development.
+        </p>
 
         <div className="container-fluid">
           <div className="row">
             <Skill name="Ruby on Rails" icon="devicon-rails-plain" />
             <Skill name="React" icon="devicon-react-plain" />
             <Skill name="AngularJS" icon="devicon-angularjs-plain" />
+            <Skill name="JavaScript ES6" icon="devicon-javascript-plain" />
+            <Skill name="Bootstrap" icon="devicon-bootstrap-plain" />
             <Skill name="HTML5" icon="devicon-html5-plain" />
             <Skill name="CSS3" icon="devicon-css3-plain" />
             <Skill name="SASS" icon="devicon-sass-plain" />
+            <Skill name="PostgreSQL" icon="devicon-postgresql-plain" />
+            <Skill name="Git/GitHub" icon="devicon-github-plain" />
+            <Skill name="C/C++/C#" icon="devicon-csharp-plain" />
+            <Skill name=".Net" icon="devicon-dot-net-plain" />
           </div>
         </div>
       </div>
